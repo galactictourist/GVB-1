f8d34bede7a7504dd1b22ebc1ecc4bdfbfbdb04f
Ngoc <12643334-ngoc.n4g@users.noreply.gitlab.com>
2022-10-25 00:13:15 +0700

    add database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,35 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSourceOptions } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { configs } from './config';
+import { IDatabaseConfig } from './config/database.config';
+import { ConfigNamespace } from './types/config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ load: configs })],
+  imports: [
+    ConfigModule.forRoot({ load: configs }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService): DataSourceOptions => {
+        const databaseConfig = configService.getOrThrow<IDatabaseConfig>(
+          ConfigNamespace.DATABASE,
+        );
+        return {
+          type: 'postgres',
+          host: databaseConfig.host,
+          port: databaseConfig.port,
+          username: databaseConfig.username,
+          password: databaseConfig.password,
+          database: databaseConfig.database,
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
