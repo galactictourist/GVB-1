9bb3c8de459f7cd5050bf376d86caf18dc3e2e09
Ngoc <12643334-ngoc.n4g@users.noreply.gitlab.com>
2022-11-05 17:09:14 +0700

    fix NftRepository

--- src/nft/repository/nft.repository.ts
+++ src/nft/repository/nft.repository.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
-import { CollectionEntity } from '../entity/collection.entity';
 import { NftEntity } from '../entity/nft.entity';
 
 @Injectable()
 export class NftRepository extends Repository<NftEntity> {
   constructor(private dataSource: DataSource) {
-    super(CollectionEntity, dataSource.createEntityManager());
+    super(NftEntity, dataSource.createEntityManager());
   }
 }
