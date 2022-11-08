adb7b4a733f7c59e348d97120252325001e81c15
Ngoc <12643334-ngoc.n4g@users.noreply.gitlab.com>
2022-11-09 00:09:58 +0700

    update seeding

--- src/database/database-seed.command.ts
+++ src/database/database-seed.command.ts
@@ -5,8 +5,9 @@ import { CharityRepository } from '~/charity/repository/charity.repository';
 import { TopicRepository } from '~/charity/repository/topic.repository';
 import { CollectionRepository } from '~/nft/repository/collection.repository';
 import { NftRepository } from '~/nft/repository/nft.repository';
+import { UserStatus } from '~/user/types';
 import { UserRepository } from '../user/repository/user.repository';
-import { userSeed } from './seeds/user.seed';
+import { createUsers } from './seeds/user.seed';
 
 @Command({ name: 'db:seed', description: 'Seed database' })
 export class DatabaseSeedCommand extends CommandRunner {
@@ -28,18 +29,16 @@ export class DatabaseSeedCommand extends CommandRunner {
   }
 
   async clear() {
-    await Promise.all([
-      this.userRepository.deleteAll(),
-      this.nonceRepository.deleteAll(),
-      this.nftRepository.deleteAll(),
-      this.collectionRepository.deleteAll(),
-      this.charityTopicRepository.deleteAll(),
-      this.topicRepository.deleteAll(),
-      this.charityRepository.deleteAll(),
-    ]);
+    await this.userRepository.deleteAll();
+    await this.nonceRepository.deleteAll();
+    await this.nftRepository.deleteAll();
+    await this.collectionRepository.deleteAll();
+    await this.charityTopicRepository.deleteAll();
+    await this.topicRepository.deleteAll();
+    await this.charityRepository.deleteAll();
   }
 
   async seed() {
-    await userSeed();
+    await createUsers({ status: UserStatus.ACTIVE }, 100);
   }
 }
--- src/database/seeds/user.seed.ts
+++ src/database/seeds/user.seed.ts
@@ -1,21 +1,44 @@
+import { utils, Wallet } from 'ethers';
+import _ from 'lodash';
 import { DeepPartial } from 'typeorm';
-import { generateWallets } from '~/lib/web3';
 import { UserEntity } from '~/user/entity/user.entity';
 
-export async function userSeed(count = 200) {
-  const wallets = generateWallets(
-    'beyond left mechanic arrow federal long recycle promote reform doctor plate elder',
-    count,
+export function createUserEntity(oriData: DeepPartial<UserEntity> = {}) {
+  const data: DeepPartial<UserEntity> = { ...oriData };
+  data.wallet = data.wallet ?? generateWallet().address;
+  return UserEntity.create(data);
+}
+
+export async function createUser(data: DeepPartial<UserEntity> = {}) {
+  const user = await UserEntity.save(createUserEntity(data));
+  return user;
+}
+
+let walletIndex = 0;
+
+const hdNode = utils.HDNode.fromMnemonic(
+  'beyond left mechanic arrow federal long recycle promote reform doctor plate elder',
+);
+
+function generateWallet(index?: number) {
+  let node;
+  if (index === undefined) {
+    node = hdNode.derivePath(`m/44'/60'/0'/0/${walletIndex}`);
+    walletIndex++;
+  } else {
+    node = hdNode.derivePath(`m/44'/60'/0'/0/${index}`);
+  }
+  return new Wallet(node.privateKey);
+}
+
+export async function createUsers(
+  data: DeepPartial<UserEntity> = {},
+  count = 1,
+) {
+  const users: DeepPartial<UserEntity>[] = _.range(count).map(() =>
+    createUserEntity(data),
   );
 
-  const data: DeepPartial<UserEntity>[] = wallets.map((wallet) => {
-    return {
-      wallet: wallet.address,
-    };
-  });
-
-  console.debug(data);
-  const users = await UserEntity.save(data);
-  console.debug(users);
-  return users;
+  const userEntities = await UserEntity.save(users);
+  return userEntities;
 }
