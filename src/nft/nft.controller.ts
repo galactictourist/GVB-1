be6ee66fee1ebd8c8f5759b8b54be7d537bc7e1b
Ngoc <12643334-ngoc.n4g@users.noreply.gitlab.com>
2022-11-15 09:42:07 +0700

    search nfts

--- src/nft/nft.controller.ts
+++ src/nft/nft.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { Public } from '~/auth/decorator/public.decorator';
 import { BlockchainNetwork } from '~/types/blockchain';
-import { formatResponse } from '~/types/response-data';
+import { formatResponse, ResponseData } from '~/types/response-data';
+import { FilterNftDto } from './dto/filter-nft.dto';
 import { NftService } from './nft.service';
+import { NftStatus } from './types';
 
 @Controller('nfts')
 export class NftController {
   constructor(private readonly nftService: NftService) {}
+
+  @Public()
+  @Post('_search')
+  async search(
+    @Body() searchNftDto: FilterNftDto,
+  ): Promise<ResponseData<any[]>> {
+    const nfts = await this.nftService.search(searchNftDto, {
+      status: NftStatus.ACTIVE,
+    });
+    return formatResponse(nfts.data, {
+      pagination: {
+        total: nfts.total,
+      },
+    });
+  }
 
   @Public()
   @Get(':network/:scAddress/:tokenId')
--- src/nft/nft.service.ts
+++ src/nft/nft.service.ts
@@ -1,10 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { FindOptionsWhere, In } from 'typeorm';
 import { BlockchainNetwork } from '~/types/blockchain';
+import { FilterNftDto } from './dto/filter-nft.dto';
+import { NftEntity } from './entity/nft.entity';
 import { NftRepository } from './repository/nft.repository';
 
 @Injectable()
 export class NftService {
   constructor(private readonly nftRepository: NftRepository) {}
+
+  async search(
+    searchNftDto: FilterNftDto,
+    defaults: FindOptionsWhere<NftEntity>,
+  ) {
+    const where: FindOptionsWhere<NftEntity> = {
+      ...defaults,
+    };
+    if (searchNftDto.ownerIds && searchNftDto.ownerIds.length) {
+      where.ownerId = In(searchNftDto.ownerIds);
+    }
+    console.log('where', where);
+
+    const [data, total] = await this.nftRepository.findAndCountBy(where);
+    return { data, total };
+  }
 
   async getNftByNetworkAddressTokenId(
     network: BlockchainNetwork,
