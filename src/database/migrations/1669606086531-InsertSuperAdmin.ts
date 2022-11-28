6737429d2f33af04e72a43be6e3c3969f79df5a0
Ngoc <12643334-ngoc.n4g@users.noreply.gitlab.com>
2022-11-28 10:33:14 +0700

    insert root account

---
+++ src/database/migrations/1669606086531-InsertSuperAdmin.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { AdminRole, AdminStatus } from '~/user/types';
+
+export class InsertSuperAdmin1669606086531 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `INSERT INTO "admin" ("username", "password", "role", "status") VALUES ('root', '$2a$12$2AQML.1jOi1sG58ZbpMrSus6cGgIi/aWGo663QwMU82Jz4HquiQTW', '${AdminRole.SUPER_ADMIN}', '${AdminStatus.ACTIVE}');`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DELETE FROM "admin" WHERE "username" = 'root'`);
+  }
+}
