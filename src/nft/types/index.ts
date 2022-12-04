67d54f46a3e0eae47602c555e246b4d884d165f7
Ngoc <12643334-ngoc.n4g@users.noreply.gitlab.com>
2022-12-04 22:02:12 +0700

    add interfaces

--- src/nft/types/index.ts
+++ src/nft/types/index.ts
@@ -7,3 +7,33 @@ export enum NftStatus {
   ACTIVE = 'ACTIVE',
   INACTIVE = 'INACTIVE',
 }
+
+export enum DisplayType {
+  BOOST_NUMBER = 'boost_number',
+  BOOST_PERCENTAGE = 'boost_percentage',
+  NUMBER = 'number',
+  DATE = 'date',
+}
+
+export interface MetadataAttribute {
+  trait_type?: string;
+  value: string | number;
+  max_value?: number;
+  display_type?: DisplayType;
+}
+
+export interface MetadataDto {
+  name?: string;
+
+  description?: string;
+
+  external_url?: string;
+
+  animation_url?: string;
+
+  youtube_url?: string;
+
+  background_color?: string;
+
+  attributes?: MetadataAttribute[];
+}
