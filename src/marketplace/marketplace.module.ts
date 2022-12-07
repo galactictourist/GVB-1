981b00439290647d1edafaa4ea7e7933b3129e63
Ngoc <12643334-ngoc.n4g@users.noreply.gitlab.com>
2022-12-07 09:32:31 +0700

    fix missing orderservice

--- src/marketplace/marketplace.module.ts
+++ src/marketplace/marketplace.module.ts
@@ -4,6 +4,7 @@ import { OrderEntity } from './entity/order.entity';
 import { SaleEntity } from './entity/sale.entity';
 import { MarketplaceService } from './marketplace.service';
 import { OrderController } from './order.controller';
+import { OrderService } from './order.service';
 import { OrderRepository } from './repository/order.repository';
 import { SaleRepository } from './repository/sale.repository';
 import { SaleController } from './sale.controller';
@@ -12,6 +13,12 @@ import { SaleService } from './sale.service';
 @Module({
   imports: [TypeOrmModule.forFeature([SaleEntity, OrderEntity])],
   controllers: [SaleController, OrderController],
-  providers: [MarketplaceService, OrderRepository, SaleRepository, SaleService],
+  providers: [
+    MarketplaceService,
+    OrderRepository,
+    SaleRepository,
+    SaleService,
+    OrderService,
+  ],
 })
 export class MarketplaceModule {}
