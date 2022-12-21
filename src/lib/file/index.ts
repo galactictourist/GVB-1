f3d5b7782b4facd9754fce424eac5a88d39c4b2c
Ngoc <12643334-ngoc.n4g@users.noreply.gitlab.com>
2022-12-21 14:32:36 +0700

    update file. rename

--- src/lib/file/index.ts
+++ src/lib/file/index.ts
@@ -1,3 +1,5 @@
 export function cleanUpFilename(filename: string) {
-  return filename.replace(/([^a-z0-9\-_\.]+)/gi, '-').replace(/(-{2,})/g, '-');
+  return filename
+    .replace(/([^a-z0-9\-_\.]+)/gi, '-')
+    .replace(/([\-_]{2,})/g, '-');
 }
