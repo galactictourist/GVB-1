873f73f1de26c2edd09f9da4dd188331f4f21b05
Ngoc <12643334-ngoc.n4g@users.noreply.gitlab.com>
2022-12-21 20:48:56 +0700

    use monorepo

--- apps/main/src/config/jwt.config.ts
+++ apps/main/src/config/jwt.config.ts
@@ -26,25 +26,28 @@ export const jwtConfig = registerAs(
     publicKey:
       process.env.JWT_PUBLIC_KEY ||
       readFileSync(
-        join(__dirname, '/../../secrets/jwt/jwt-rs256.pub.key'),
+        join(__dirname, '/../../../secrets/jwt/jwt-rs256.pub.key'),
         'utf8',
       ),
     privateKey:
       process.env.JWT_PRIVATE_KEY ||
-      readFileSync(join(__dirname, '/../../secrets/jwt/jwt-rs256.key'), 'utf8'),
+      readFileSync(
+        join(__dirname, '/../../../secrets/jwt/jwt-rs256.key'),
+        'utf8',
+      ),
     admin: {
       algo: 'RS256',
       expiresIn: process.env.JWT_ADMIN_EXPIRES_IN || '1h',
       publicKey:
         process.env.JWT_ADMIN_PUBLIC_KEY ||
         readFileSync(
-          join(__dirname, '/../../secrets/jwt-admin/jwt-rs256.pub.key'),
+          join(__dirname, '/../../../secrets/jwt-admin/jwt-rs256.pub.key'),
           'utf8',
         ),
       privateKey:
         process.env.JWT_ADMIN_PRIVATE_KEY ||
         readFileSync(
-          join(__dirname, '/../../secrets/jwt-admin/jwt-rs256.key'),
+          join(__dirname, '/../../../secrets/jwt-admin/jwt-rs256.key'),
           'utf8',
         ),
     },
