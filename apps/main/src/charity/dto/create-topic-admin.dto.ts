d60882e6bcf0441d6b45940b6d9fda8e5ecd680f
Ngoc <12643334-ngoc.n4g@users.noreply.gitlab.com>
2023-02-21 05:31:21 +0700

    support topic status

--- apps/main/src/charity/dto/create-topic-admin.dto.ts
+++ apps/main/src/charity/dto/create-topic-admin.dto.ts
@@ -1,7 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsDefined, IsOptional, IsString, IsUUID } from 'class-validator';
+import {
+  IsDefined,
+  IsEnum,
+  IsOptional,
+  IsString,
+  IsUUID,
+} from 'class-validator';
 import { StorageLabel } from '~/main/storage/types';
 import { IsStorageId } from '~/main/storage/validator/is-storage-id.validator';
+import { TopicStatus } from '../types';
 
 export class CreateTopicAdminDto {
   @ApiProperty()
@@ -13,6 +20,10 @@ export class CreateTopicAdminDto {
   @IsOptional()
   @IsUUID('4')
   parentId?: string;
+
+  @ApiProperty({ enum: TopicStatus })
+  @IsEnum(TopicStatus)
+  status: TopicStatus;
 
   @ApiProperty({ nullable: true, required: false })
   @IsOptional()
